8ab0b0e6feeef8a2de88964c72787a51195a6260
Ujjwal Kandera <ujjwalkandera23@gmail.com>
2021-04-26 12:56:46 +0530

    Update gsetup.py, add leave command

--- Commands/gsetup.py
+++ Commands/gsetup.py
@@ -15,9 +15,10 @@ class SetupCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # Async function to check if the author is already entered in a record
-    async def check_inGame(self, ctx):
-        check = cur.execute('SELECT * FROM players WHERE playerId = "{}"'.format(ctx.author.id))
+    # Function to check if the author is already entered in a record
+    def check_inGame(self, ctx):
+        check = cur.execute('SELECT * FROM players WHERE playerId = "{}"'
+        .format(ctx.author.id))
         if check.fetchone():
             return True
         else:
@@ -25,29 +26,75 @@ class SetupCog(commands.Cog):
 
     # Used to create a New L's Game
     @commands.command()
-    async def create(self, ctx):
-        if await self.check_inGame(ctx) == True:
+    async def create(self, ctx, g_channel : discord.TextChannel):
+        check_guildGame = cur.execute("SELECT guildId FROM games WHERE guildId = '{}'"
+        .format(ctx.guild.id)).fetchone()
+
+        if self.check_inGame(ctx) == True:
             await ctx.send('You have already joined another game')
+        elif check_guildGame:
+            await ctx.send('An L\'s game is already ongoing in this server')
         else:
             # Adds the Author's data into the database
-            cur.execute('INSERT INTO players (playerId, gameId) VALUES ("{}", "{}")'.format(ctx.author.id, game_id))
-            cur.execute('INSERT INTO games (masterId, gameId, guildId, "state") VALUES ("{}", "{}", "{}", "pre-game")'.format(ctx.author.id, game_id, ctx.guild.id))
+            cur.execute('INSERT INTO players (playerId, gameId) VALUES ("{}", "{}")'
+            .format(ctx.author.id, game_id))
+            cur.execute('INSERT INTO games (masterId, gameId, guildId, "state", channelId) VALUES ("{}", "{}", "{}", "pre-game", "{}")'
+            .format(ctx.author.id, game_id, ctx.guild.id, g_channel.id))
 
-            await ctx.send('An L\'s Game has been created with Game ID : **{}**'.format(game_id))
+            await ctx.send('An L\'s Game has been created with Game ID : **{}**'
+            .format(game_id))
 
             con.commit()
     
     # Used to join an existing game
     @commands.command()
     async def join(self, ctx):
-        if await self.check_inGame(ctx) == True:
+        if self.check_inGame(ctx) == True:
             await ctx.send('You have already joined another game')
         else:
-            g_id = cur.execute("SELECT gameId FROM games WHERE guildId = '{}'".format(ctx.guild.id)).fetchone()[0]
-            cur.execute('INSERT INTO players (playerId, gameId) VALUES ("{}", "{}")'.format(ctx.author.id, g_id))
+            g_id = cur.execute("SELECT gameId FROM games WHERE guildId = '{}'"
+            .format(ctx.guild.id)).fetchone()[0]
+            cur.execute('INSERT INTO players (playerId, gameId) VALUES ("{}", "{}")'
+            .format(ctx.author.id, g_id))
 
-            await ctx.send('Sucessfully joined L\'s Game with Game ID : **{}**'.format(g_id))
+            await ctx.send('Sucessfully joined L\'s Game with Game ID : **{}**'
+            .format(g_id))
+
+            con.commit()
+        
+    # Used to leave a joined L's Game
+    @commands.command()
+    async def leave(self, ctx):
+        if self.check_inGame(ctx) == False:
+            await ctx.send('You have not participated in an L\'s Game')
+        else:
+            g_id = cur.execute("SELECT gameId FROM games WHERE guildId = '{}'"
+            .format(ctx.guild.id)).fetchone()[0]
+            cur.execute("DELETE FROM players WHERE playerId = '{}' AND gameId = '{}'"
+            .format(ctx.author.id, g_id))
+
+            await ctx.send('Sucessfully left L\'s Game with Game ID : **{}**'
+            .format(g_id))
+
             con.commit()
 
+# W.I.P (Incomplete)
+"""
+    @commands.command()
+    async def start(self, ctx):
+        gamemaster_id = cur.execute("SELECT masterId FROM games WHERE guildId = '{}'"
+        .format(ctx.guild.id)).fetchone()
+
+        gamemaster_user = self.bot.get_user(gamemaster_id)
+
+        if self.check_inGame(ctx) == False:
+            await ctx.send('You are not in a L\'s Game')
+        elif not gamemaster_id == ctx.author.id:
+            await ctx.send('Only the Gamemaster : {} can start the game'
+            .format(gamemaster_user.name))
+        else:
+            cur.execute("UPDATE games SET state = 'gameplay' WHERE guildId = '{}'"
+            .format(ctx.guild.id))
+"""
 def setup(bot):
     bot.add_cog(SetupCog(bot))
